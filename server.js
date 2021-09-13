4ca76e50d1cc28d53401e7a9ea6f40601c3c6e3f
tanzumt <89188092+tanzumt@users.noreply.github.com>
2021-09-13 19:24:28 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/workspace', (request, response) => {
   response.send(`<!DOCTYPE html>
 <html>
   <head>
     <title>Powered By Paketo Buildpacks</title>
   </head>
   <body>
-  
-    <img src="./paketo-logo-full-color.png"></img>
+    <img src="paketo-logo-full-color.png"></img>
   </body>
 </html>`);
 });
