4544015c1472ba9cabc54e38c54d25857cd105e6
tanzumt <89188092+tanzumt@users.noreply.github.com>
2021-09-13 18:41:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', (request, response) => {
     <title>Powered By Paketo Buildpacks</title>
   </head>
   <body>
-    <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="./paketo-logo-full-color.png"></img>
+    <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="paketo-logo-full-color.png"></img>
   </body>
 </html>`);
 });
